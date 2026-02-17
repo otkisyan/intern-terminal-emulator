ad8df04e0e9c26c18f1ef21e3c0af1f2a3b8d33c
otkisyan <achiev69@gmail.com>
2026-02-17 18:03:18 +0100

    test: add edit operations tests

--- src/test/java/TerminalBufferTest.java
+++ src/test/java/TerminalBufferTest.java
@@ -41,6 +41,20 @@ class TerminalBufferTest {
     }
 
     @Test
+    @DisplayName("setCursor() should clamp cursor within screen boundaries")
+    void setCursorShouldRespectBounds() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
+
+        buffer.setCursor(-10, -10);
+        assertEquals(0, buffer.getCursorRow());
+        assertEquals(0, buffer.getCursorCol());
+
+        buffer.setCursor(100, 100);
+        assertEquals(2, buffer.getCursorRow());
+        assertEquals(4, buffer.getCursorCol());
+    }
+
+    @Test
     @DisplayName("moveUp/moveDown/moveLeft/moveRight should move cursor correctly within bounds")
     void moveCursorShouldUpdatePosition() {
         TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
@@ -65,7 +79,7 @@ class TerminalBufferTest {
     }
 
     @Test
-    @DisplayName("move*() should allow cursor to be moved to screen boundaries")
+    @DisplayName("moveUp/moveDown/moveLeft/moveRight should allow cursor to be moved to screen boundaries")
     void moveCursorFromZeroToEdges() {
         TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
 
@@ -92,20 +106,6 @@ class TerminalBufferTest {
     }
 
     @Test
-    @DisplayName("setCursor() should clamp cursor within screen boundaries")
-    void setCursorShouldRespectBounds() {
-        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
-
-        buffer.setCursor(-10, -10);
-        assertEquals(0, buffer.getCursorRow());
-        assertEquals(0, buffer.getCursorCol());
-
-        buffer.setCursor(100, 100);
-        assertEquals(2, buffer.getCursorRow());
-        assertEquals(4, buffer.getCursorCol());
-    }
-
-    @Test
     @DisplayName("moveUp/moveDown/moveLeft/moveRight should clamp cursor within screen boundaries")
     void moveCursorShouldRespectBounds() {
         TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
@@ -121,6 +121,159 @@ class TerminalBufferTest {
 
         buffer.moveRight(10);
         assertEquals(4, buffer.getCursorCol());
+    }
+
+    @Test
+    @DisplayName("write() should overwrite existing characters at cursor position")
+    void writeShouldOverwriteCharacters() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
+
+        buffer.write("HELLO");
+        buffer.setCursor(0, 0);
+        buffer.write("X");
+
+        assertEquals("XELLO", buffer.getLine(0, false));
+        buffer.write("XXXX");
+        assertEquals("XXXXX", buffer.getLine(0, false));
+    }
+
+    @Test
+    @DisplayName("write() should wrap to next line when reaching screen width")
+    void writeShouldWrapToNextLineWhenWidthExceeded() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
+
+        buffer.write("ABCDEFG");
+
+        assertEquals("ABCDE", buffer.getLine(0, false));
+        assertEquals("FG   ", buffer.getLine(1, false));
+    }
+
+    @Test
+    @DisplayName("write() should scroll screen up when writing beyond screen height")
+    void writeShouldScrollWhenHeightExceeded() {
+        TerminalBuffer buffer = new TerminalBuffer(3, 2, 10);
+
+        buffer.write("123456789");
+
+        assertEquals("456", buffer.getLine(0, false));
+        assertEquals("789", buffer.getLine(1, false));
+    }
+
+    @Test
+    @DisplayName("insert() should shift characters to the right from cursor position")
+    void insertShouldShiftCharactersRight() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
+
+        buffer.write("HELLO");
+        buffer.setCursor(0, 1);
+        buffer.insert("X");
+
+        assertEquals("HXELL", buffer.getLine(0, false));
+    }
+
+    @Test
+    @DisplayName("insert() at last column should overwrite character (no shift possible)")
+    void insertAtLastColumnShouldOverwrite() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 3, 10);
+
+        buffer.write("HELLO");
+        buffer.setCursor(0, 4);
+        buffer.insert("X");
+
+        assertEquals("HELLX", buffer.getLine(0, false));
+    }
+
+    @Test
+    @DisplayName("insert() should wrap text to next line when width exceeded")
+    void insertShouldWrapToNextLine() {
+        TerminalBuffer buffer = new TerminalBuffer(3, 2, 10);
+
+        buffer.insert("ABCDE");
+
+        assertEquals("ABC", buffer.getLine(0, false));
+        assertEquals("DE ", buffer.getLine(1, false));
+    }
+
+    @Test
+    @DisplayName("insertEmptyLineAtBottom() should scroll top line into scrollback and add empty line at bottom")
+    void insertEmptyLineAtBottomShouldScroll() {
+        TerminalBuffer buffer = new TerminalBuffer(3, 2, 10);
+
+        buffer.write("ABC");
+        buffer.write("DEF");
+
+        buffer.insertEmptyLineAtBottom();
+
+        assertEquals("ABC", buffer.getLine(0, true));
+
+        assertEquals("DEF", buffer.getLine(0, false));
+        assertEquals("   ", buffer.getLine(1, false));
+    }
+
+    @Test
+    @DisplayName("fillLine() should replace entire row with given character")
+    void fillLineShouldReplaceEntireRow() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
+
+        buffer.fillLine(0, 'X');
+
+        assertEquals("XXXXX", buffer.getLine(0, false));
+    }
+
+    @Test
+    @DisplayName("scrollUp() should move top screen line into scrollback")
+    void scrollbackShouldStoreScrolledLines() {
+        TerminalBuffer buffer = new TerminalBuffer(3, 2, 10);
+
+        buffer.write("123456789");
+
+        assertEquals("123", buffer.getLine(0, true));
+        assertEquals("456", buffer.getLine(0, false));
+        assertEquals("789", buffer.getLine(1, false));
+    }
+
+    @Test
+    @DisplayName("scrollUp() should discard oldest lines when exceeding scrollbackMax")
+    void scrollbackShouldRespectMaxSize() {
+        TerminalBuffer buffer = new TerminalBuffer(3, 2, 1);
+
+        buffer.write("123456789000");
+
+        assertEquals("456", buffer.getLine(0, true));
+    }
+
+    @Test
+    @DisplayName("clearScreen() should clear visible screen but keep scrollback")
+    void clearScreenShouldResetScreenOnly() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 2, 10);
+
+        buffer.write("HELLO\nWORLD");
+
+        assertEquals("HELLO", buffer.getLine(0, true));
+
+        buffer.clearScreen();
+
+        assertEquals("     ", buffer.getLine(0, false));
+        assertEquals("     ", buffer.getLine(1, false));
+
+        assertEquals("HELLO", buffer.getLine(0, true));
+    }
+
+    @Test
+    @DisplayName("clearAll() should clear both screen and scrollback")
+    void clearAllShouldClearScrollbackToo() {
+        TerminalBuffer buffer = new TerminalBuffer(3, 2, 10);
+
+        buffer.write("123456789");
+
+        assertEquals("123", buffer.getLine(0, true));
+
+        buffer.clearAll();
+
+        assertEquals("   ", buffer.getLine(0, false));
+        assertEquals("   ", buffer.getLine(1, false));
+
+        assertEquals("", buffer.getLine(0, true));
     }
 }
 
